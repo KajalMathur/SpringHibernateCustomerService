db4543fac2c815ee6b861e15fb7454e88db8bcfb
Kajal <kajalmathur05@gmail.com>
2019-12-03 00:53:42 +0530

    Eureka Ribbon and Zuul

--- src/main/java/com/customer/CustomerMain.java
+++ src/main/java/com/customer/CustomerMain.java
@@ -2,12 +2,13 @@ package com.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomerMain {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(CustomerMain.class, args);
 	}
 }
