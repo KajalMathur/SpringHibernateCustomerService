01717d85c2755beaf8fad20ee26365777ebf98a4
Kajal <kajalmathur05@gmail.com>
2020-04-01 23:25:52 -0400

    Addressed PR Comments

--- src/test/java/com/customer/customerService/CrediCardApplicationTest.java
+++ src/test/java/com/customer/customerService/CrediCardApplicationTest.java
@@ -4,49 +4,47 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import com.customer.model.CreditCard;
 import com.customer.repository.CreditCardRepository;
 import com.customer.service.CreditCardServiceImpl;
-
-
+import com.google.common.collect.ImmutableList;
 
 public class CrediCardApplicationTest {
 	
 	CreditCard creditCardData, creditCardDataWithNull, creditCardData1, creditCardData2, creditCardData3, creditCardData4;
 	
-	List<CreditCard> creditCardList;
-	
 	CreditCardRepository creditCardRepositoryMock;
 	
 	CreditCardServiceImpl creditCardServiceImpl;
 	
-	List<CreditCard> creditCardList1;
+	ImmutableList<CreditCard> creditCardList;
 	
-	List<CreditCard> creditCardList2;
+	ImmutableList<CreditCard> creditCardList1;
 	
-	List<CreditCard> creditCardEmptyList;
+	ImmutableList<CreditCard> creditCardList2;
 	
-	List<CreditCard> creditCardSortedList;
+	ImmutableList<CreditCard> creditCardEmptyList;
+	
+	ImmutableList<CreditCard> creditCardSortedList;
 	
 	@Before
 	public void dataCreation() {
+		
+		creditCardRepositoryMock = mock(CreditCardRepository.class);
+		
+		creditCardServiceImpl = new CreditCardServiceImpl(creditCardRepositoryMock);
+		
 		creditCardData = CreditCard.builder()
+				.id(10)
 				.cardHolderName("DemoTest")
 				.cardNumber("2345678123456101")
 				.cvv("123")
 				.expiryMonth(8)
 				.expiryYear(2025)
 				.build();
-		
-		creditCardRepositoryMock = mock(CreditCardRepository.class);
-		
-		creditCardServiceImpl = new CreditCardServiceImpl(creditCardRepositoryMock);
 		
 		creditCardData1 = CreditCard.builder()
 				.id(1)
@@ -93,36 +91,22 @@ public class CrediCardApplicationTest {
 				.expiryYear(2021)
 				.build();
 		
-		creditCardList = new ArrayList<CreditCard>();
-		creditCardList.add(creditCardData);
-		creditCardList.add(creditCardData1);
-		creditCardList.add(creditCardData2);
-		creditCardList.add(creditCardData3);
-		creditCardList.add(creditCardData4);
+		creditCardList = ImmutableList.of(creditCardData, creditCardData1, creditCardData2, creditCardData3, creditCardData4);
 		
-		creditCardList1 = new ArrayList<CreditCard>();
-		creditCardList1.add(creditCardData);
+		creditCardList1 = ImmutableList.of(creditCardData);
 		
-		creditCardList2 = new ArrayList<CreditCard>();
-		creditCardList2.add(creditCardData);
-		creditCardList2.add(creditCardData1);
-		creditCardList2.add(creditCardData2);
-		creditCardList2.add(creditCardData3);
-		creditCardList2.add(creditCardData4);
+	
+		creditCardList2 = ImmutableList.of(creditCardData, creditCardData1, creditCardData2, creditCardData3, creditCardData4);
 		
-		creditCardEmptyList = new ArrayList<CreditCard>();
+		creditCardEmptyList = ImmutableList.of();
 		
-		creditCardSortedList = new ArrayList<CreditCard>();
-		creditCardSortedList.add(creditCardData4);
-		creditCardSortedList.add(creditCardData2);
-		creditCardSortedList.add(creditCardData1);
-		creditCardSortedList.add(creditCardData3);
+		creditCardSortedList = ImmutableList.of(creditCardData4, creditCardData2, creditCardData1, creditCardData3);
 	}
 	
 	/* Credit Card post request testcases */
 	
 	@Test
-	 public void verifyCreditCardPostwithSuccess() {
+	public void verifyCreditCardPostwithSuccess() {
 	        when(creditCardRepositoryMock.save(creditCardData)).thenReturn(creditCardData);
 	        assertEquals(creditCardServiceImpl.createCreditCardInfo(creditCardData),creditCardData);
 	 }
@@ -136,7 +120,7 @@ public class CrediCardApplicationTest {
 	 @Test
 	 public void verifyCreditCardPostWithoneNullValue() {
 		 when(creditCardRepositoryMock.save(creditCardDataWithNull)).thenReturn(creditCardDataWithNull);
-	        assertEquals(creditCardServiceImpl.createCreditCardInfo(creditCardDataWithNull),creditCardDataWithNull);
+	     assertEquals(creditCardServiceImpl.createCreditCardInfo(creditCardDataWithNull),creditCardDataWithNull);
 	 }
 	 
 	 /* Credit Card get request testcases */
